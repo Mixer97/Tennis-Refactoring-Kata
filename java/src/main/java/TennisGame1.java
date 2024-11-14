e21fe679ca840753207b635a2ac1a77ddbe06497
Mixer97 <169163595+Mixer97@users.noreply.github.com>
2024-11-14 12:36:45 +0100

    Starting to remove hard coded player names

--- java/src/main/java/TennisGame1.java
+++ java/src/main/java/TennisGame1.java
@@ -3,8 +3,8 @@ public class TennisGame1 implements TennisGame {
 
     private int m_score1 = 0;
     private int m_score2 = 0;
-    private String player1Name;
-    private String player2Name;
+    private final String player1Name;
+    private final String player2Name;
 
     public TennisGame1(String player1Name, String player2Name) {
         this.player1Name = player1Name;
@@ -12,7 +12,7 @@ public class TennisGame1 implements TennisGame {
     }
 
     public void wonPoint(String playerName) {
-        if (playerName.equals("player1"))
+        if (playerName.equals(player1Name))
             m_score1 += 1;
         else
             m_score2 += 1;
