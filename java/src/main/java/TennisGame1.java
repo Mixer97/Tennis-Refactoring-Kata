2147598d3aad4c4044d4068cf68bf67678ff8816
Mixer97 <169163595+Mixer97@users.noreply.github.com>
2024-11-14 12:38:41 +0100

    Remove all hard coded player names

--- java/src/main/java/TennisGame1.java
+++ java/src/main/java/TennisGame1.java
@@ -30,10 +30,10 @@ public class TennisGame1 implements TennisGame {
             };
         } else if (m_score1 >= 4 || m_score2 >= 4) {
             int minusResult = m_score1 - m_score2;
-            if (minusResult == 1) score = "Advantage player1";
-            else if (minusResult == -1) score = "Advantage player2";
-            else if (minusResult >= 2) score = "Win for player1";
-            else score = "Win for player2";
+            if (minusResult == 1) score = "Advantage ".concat(player1Name);
+            else if (minusResult == -1) score = "Advantage ".concat(player2Name);
+            else if (minusResult >= 2) score = "Win for ".concat(player1Name);
+            else score = "Win for ".concat(player2Name);
         } else {
             for (int i = 1; i < 3; i++) {
                 if (i == 1) tempScore = m_score1;
